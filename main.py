bb47fdd8147a086f7f46a65a6cb662e6cf5b1451
ikk <ikk@ikknoMacBook-Pro.local>
2020-09-27 17:28:18 +0900

    test_87

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ importlib.reload(sudoku)
 # In[2]:
 
 
-result = np.loadtxt('input_data/test_91.csv', delimiter=',', dtype='int64')
+result = np.loadtxt('input_data/test_87.csv', delimiter=',', dtype='int64')
 sudoku = sudoku.Sudoku(result)
 
 
@@ -44,4 +44,28 @@ sudoku.display_result()
 
 
 sudoku.display_remaining_candidate()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
